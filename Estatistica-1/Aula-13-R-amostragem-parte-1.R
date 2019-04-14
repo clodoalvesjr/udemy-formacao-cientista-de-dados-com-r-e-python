0cc5d8385f00b927694a4ffa78875cb76bcf1630
José Clodoalves <joseclodoalves@gmail.com>
2019-04-14 10:40:42 -0300

    Criacao de projeto e de primeiro script

---
+++ Estatistica-1/Aula-13-R-amostragem-parte-1.R
@@ -0,0 +1,8 @@
+#Funcao utilizada para geracao de amostra aleatoria simples com ou sem repeticao
+#sample()
+
+#Funcao para geracao de amostra estratificada
+#strata()
+
+#Funcao para geracao de amostra sistematica
+#S.SY()
