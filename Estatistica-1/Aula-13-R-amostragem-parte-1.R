6447b562544884ecc96d4e31fb0c98515d21ef64
José Clodoalves <joseclodoalves@gmail.com>
2019-04-20 15:14:39 -0300

    Criacao de script da aula 13

--- Estatistica-1/Aula-13-R-amostragem-parte-1.R
+++ Estatistica-1/Aula-13-R-amostragem-parte-1.R
@@ -1,8 +1,33 @@
-#Funcao utilizada para geracao de amostra aleatoria simples com ou sem repeticao
-#sample()
+#Funcoes utilizadas neste script
 
-#Funcao para geracao de amostra estratificada
-#strata()
+# => sample() - Funcao utilizada para geracao de amostras aleatorias simples com ou sem repeticao
+#Parametros utilizados:
+# - x: Conjunto de dados para a extracao das amostras
+# - size: Quantidade de numeros aleatorios a serem gerados
+# - valor logico. Determina se o experimento eh com reposicao ou nao
+# - Vetor de probabilidade para obtencao dos elementos do conjunto amostrado
 
-#Funcao para geracao de amostra sistematica
-#S.SY()
+# => length() => Retorna a quantidade de elementos de um conjunto de dados
+# - x> #Parametros utilizados:
+# - x: Conjunto de dados
+
+# => set.seed() => Funcao para criar uma semente aleatoriedade para que um experimento ja executado
+#            possa ser repetido. Para mais informacoes, favor consultar a documentacao
+
+
+#Retorna amostra simples com repeticao com 150 elementos a partir do conjunto de dados (0, 1)
+# com probabilidade de 50% para cada elemento
+amostra = sample(c(0,1), 150, replace = TRUE, prob = c(0.5, 0.5))
+
+
+#Retorna a quantidade elementos iguais a 1 na amostra gerada
+length(amostra[amostra == 1])
+
+#Retorna a quantidade elementos iguais a 0  na amostra gerada
+length(amostra[amostra == 0])
+
+# Cria semente de aleatoriedade para que a geracao da amostra na linha 31 retorne sempre
+# o mesmo valor
+
+set.seed(123)
+sample(c(100), 1)
